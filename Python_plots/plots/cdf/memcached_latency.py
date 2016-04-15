7e38d1db353a0e117d6410dc976a03e5160ad3c8
Panagiotis Garefalakis <pangaref@gmail.com>
2016-04-15 20:13:23 +0100

    Getting started with matplotlib and CDFs

---
+++ Python_plots/plots/cdf/memcached_latency.py
@@ -0,0 +1,200 @@
+__author__ = 'pg1712'
+
+# The MIT License (MIT)
+#
+# Copyright (c) 2016 Panagiotis Garefalakis
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from datetime import datetime
+import os, sys, re
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+from plots.utils import *
+
+
+paper_mode = True
+
+# value_map = {}
+# with open(filename) as f:
+#     for line in f:
+#         values = line.strip().split(' ')
+# #        print 'key {} , Value {} '.format(values[3], values[5])
+#         # Have to convert NanoSecond timestamp-IDs stored to normal ones
+#         print 'Time {}'.format(datetime.utcfromtimestamp(float(values[3])/1000000000.0))
+#         value_map[values[3]] = values[5]
+# for v in values:
+#     try:
+#         print "TS {} Value {}", v[4] , v[6]
+#         # Do Something with x and y
+#     except IndexError:
+#         print "A line in the file doesn't have enough entries."
+
+if paper_mode:
+    colors = paper_colors
+    fig = plt.figure(figsize=(2.33,1.55))
+    set_paper_rcs()
+else:
+    colors = ['b', 'r', 'g', 'c', 'm', 'y', 'k', '0.5']
+    fig = plt.figure()
+    set_rcs()
+
+
+def plot_cdf(outname):
+
+    plt.xticks()
+    #  plt.xscale("log")
+    plt.xlim(0, 2250)
+    plt.xticks(range(0, 2500, 500), [str(x) for x in range(0, 2500, 500)])
+    plt.ylim(0, 1.0)
+    plt.yticks(np.arange(0.0, 1.01, 0.2), [str(x) for x in np.arange(0.0, 1.01, 0.2)])
+    plt.xlabel("Request latency [$\mu$s]")
+    # print n
+    # print bins
+    # plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3,
+    #           ncol=3, mode="expand", frameon=True, borderaxespad=0.)
+    plt.legend(loc=4, frameon=False, handlelength=2.5, handletextpad=0.2)
+    plt.savefig("%s.pdf" % outname, format="pdf", bbox_inches="tight")
+    plt.ylim(0.9, 1.0)
+    # plt.axhline(0.999, ls='--', color='k')
+    plt.savefig("%s-90th.pdf" % outname, format="pdf", bbox_inches="tight")
+
+
+@static_var("label_count", 0)
+def add_plt_values(values, min_val, max_val, label, req_type):
+    # figure out number of bins based on range
+    bin_width = 1  # 7.5ns measurement accuracy
+    bin_range = max_val - min_val
+    num_bins = min(250000, bin_range / bin_width)
+    print "Binning into %d bins and plotting..." % (num_bins)
+
+    # plotting
+    if paper_mode:
+        #    plt.rc("font", size=8.0)
+        label_str = label
+        if add_plt_values.label_count % 3 == 0:
+            style = 'solid'
+        elif add_plt_values.label_count % 3 == 1:
+            style = 'dashed'
+        else:
+            style = 'dotted'
+        (n, bins, patches) = plt.hist(values, bins=num_bins, log=False, normed=True,
+                                      cumulative=True, histtype="step",
+                                      ls=style, color=colors[add_plt_values.label_count])
+        # hack to add line to legend
+        plt.plot([-100], [-100], label=label,
+                 color=colors[i],
+                 linestyle=style, lw=1.0)
+        # hack to remove vertical bar
+        patches[0].set_xy(patches[0].get_xy()[:-1])
+    else:
+        label_str = "%s (%s)" % (label, req_type)
+        (n, bins, patches) = plt.hist(values, bins=num_bins, log=False, normed=True,
+                                      cumulative=True, histtype="step",
+                                      label=label_str)
+        # hack to remove vertical bar
+        patches[0].set_xy(patches[0].get_xy()[:-1])
+        #  serialize_hist(n, bins, os.path.dirname(outname))
+    add_plt_values.label_count += 1
+
+
+
+def file_parser(fnames):
+    i = 0
+    for f in fnames:
+        print "Analyzing %s:" % (f)
+
+        # parsing
+        j = 0
+        minrto_outliers = 0
+        values = {'GET': [], 'SET': [], 'TOTAL': []}
+        for line in open(f).readlines():
+            fields = [x.strip() for x in line.split()]
+            if fields[0] not in ["GET", "SET", "TOTAL"]:
+                print "Skipping line '%s'" % (line)
+                continue
+            req_type = fields[0]
+            req_id = int(fields[3])
+            req_latency = int(fields[5])
+            if req_latency > 200000:
+                minrto_outliers += 1
+            values[req_type].append(req_latency)
+            j += 1
+        print "--------------------------------------"
+        print "%s (%s)" % (labels[i], f)
+        print "--------------------------------------"
+        print "%d total samples" % (j)
+        print "%d outliers due to MinRTO" % (minrto_outliers)
+        print "--------------------------------------"
+        for type in ['TOTAL']:
+            avg = np.mean(values[type])
+            print "%s - AVG: %f" % (type, avg)
+            median = np.median(values[type])
+            print "%s - MEDIAN: %f" % (type, median)
+            min_val = np.min(values[type])
+            print "%s - MIN: %ld" % (type, min_val)
+            max_val = np.max(values[type])
+            print "%s - MAX: %ld" % (type, max_val)
+            stddev = np.std(values[type])
+            print "%s - STDEV: %f" % (type, stddev)
+            print "%s - PERCENTILES:" % (type)
+            perc1 = np.percentile(values[type], 1)
+            print " 1st: %f" % (perc1)
+            perc10 = np.percentile(values[type], 10)
+            print " 10th: %f" % (np.percentile(values[type], 10))
+            perc25 = np.percentile(values[type], 25)
+            print " 25th: %f" % (np.percentile(values[type], 25))
+            perc50 = np.percentile(values[type], 50)
+            print " 50th: %f" % (np.percentile(values[type], 50))
+            perc75 = np.percentile(values[type], 75)
+            print " 75th: %f" % (np.percentile(values[type], 75))
+            perc90 = np.percentile(values[type], 90)
+            print " 90th: %f" % (np.percentile(values[type], 90))
+            perc99 = np.percentile(values[type], 99)
+            print " 99th: %f" % (np.percentile(values[type], 99))
+
+            add_plt_values(values[type], min_val, max_val, labels[i], type)
+
+        i += 1
+
+
+if __name__ == '__main__':
+
+    if len(sys.argv) < 2:
+      print "Usage: memcached_latency_cdf.py <input file 1> <label 1> ... " \
+          "<input  file n> <label n> [output file]"
+
+    if (len(sys.argv) - 1) % 2 != 0:
+      outname = sys.argv[-1]
+    else:
+      outname = "memcached_latency_cdf"
+
+    fnames = []
+    labels = []
+    for i in range(0, len(sys.argv) - 1, 2):
+      fnames.append(sys.argv[1 + i])
+      labels.append(sys.argv[2 + i])
+
+    print 'Files given: {}'.format("".join(fname for fname in fnames))
+    print 'Labels given: {}'.format("".join(label for label in labels))
+
+    file_parser(fnames)
+    plot_cdf(outname)
---
+++ Python_plots/plots/utils.py
@@ -0,0 +1,110 @@
+__author__ = 'pg1712'
+
+from matplotlib import use, rc
+use('Agg')
+import matplotlib.pyplot as plt
+import numpy as np
+
+# plot saving utility function
+def writeout(filename_base, tight=True):
+  for fmt in ['pdf']:
+    if tight:
+      plt.savefig("%s.%s" % (filename_base, fmt), format=fmt, bbox_inches='tight')
+    else:
+      plt.savefig("%s.%s" % (filename_base, fmt), format=fmt)
+
+def set_leg_fontsize(size):
+  rc('legend', fontsize=size)
+
+def set_paper_rcs():
+  rc('font',**{'family':'sans-serif','sans-serif':['Helvetica'],
+               'serif':['Helvetica'],'size':9})
+  rc('text', usetex=True)
+  rc('legend', fontsize=8)
+  rc('figure', figsize=(3.33,2.22))
+#  rc('figure.subplot', left=0.10, top=0.90, bottom=0.12, right=0.95)
+  rc('axes', linewidth=0.5)
+  rc('lines', linewidth=0.5)
+
+def set_rcs():
+  rc('font',**{'family':'sans-serif','sans-serif':['Helvetica'],
+               'serif':['Times'],'size':12})
+  rc('text', usetex=True)
+  rc('legend', fontsize=7)
+  rc('figure', figsize=(6,4))
+  rc('figure.subplot', left=0.10, top=0.90, bottom=0.12, right=0.95)
+  rc('axes', linewidth=0.5)
+  rc('lines', linewidth=0.5)
+
+def append_or_create(d, i, e):
+  if not i in d:
+    d[i] = [e]
+  else:
+    d[i].append(e)
+
+def add_or_create(d, i, e):
+  if not i in d:
+    d[i] = e
+  else:
+    d[i] = d[i] + e
+
+
+# event log constants
+RESOURCE_UTILIZATION_SAMPLE = 0
+TX_SUCCEEDED = 1
+TX_FAILED = 2
+COLLECTION_ENDING = 3
+VMS_CHANGED_STATE = 4
+SCHEDULING_OUTCOME = 5
+COLLECTION_SUBMITTED = 6
+SCHEDULING_TIME = 7
+ZOMBIE_COLLECTION_DROPPED = 8
+OVERLAP_COLLECTION_DROPPED = 9
+COLLECTION_TRUNCATED = 10
+CELL_STATE_SETUP = 11
+END_ONLY_ENDS = 12
+
+ARRIVAL_SAMPLE = 100
+LEAVING_SAMPLE = 101
+RES_LIMIT_SAMPLE = 102
+ACTIVE_SAMPLE = 103
+COLLECTION_ARRIVING_EVENT = 104
+COLLECTION_LEAVING_EVENT = 105
+
+MAPREDUCE_PREDICTION = 200
+MAPREDUCE_ORIGINAL_RUNTIME = 201
+MAPREDUCE_RESOURCE_ADJUSTMENT = 202
+MAPREDUCE_BASE_RUNTIME = 203
+
+paper_figsize_small = (1.1, 1.1)
+paper_figsize_small_square = (1.5, 1.5)
+paper_figsize_medium = (2, 1.33)
+paper_figsize_medium_square = (2, 2)
+#paper_figsize_medium = (1.66, 1.1)
+paper_figsize_large = (3.33, 2.22)
+paper_figsize_bigsim3 = (2.4, 1.7)
+
+#8e053b red
+#496ee2 blue
+#ef9708 orange
+paper_colors = ['#496ee2', '#8e053b', 'g', '#ef9708', '0', '#eeefff', '0.5', 'c', '0.7']
+
+# -----------------------------------
+
+def think_time_fn(x, y, s):
+  return x + y * s
+
+# -----------------------------------
+
+def get_mad(median, data):
+  devs = [abs(x - median) for x in data]
+  mad = np.median(devs)
+  return mad
+
+# -----------------------------------
+
+def static_var(varname, value):
+    def decorate(func):
+        setattr(func, varname, value)
+        return func
+    return decorate
