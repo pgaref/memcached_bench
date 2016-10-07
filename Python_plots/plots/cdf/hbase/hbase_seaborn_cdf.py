87a69c032fe74785d153c96bd5ac8028e06b93e6
pgaref <pgaref@macbook-air.lan>
2016-10-07 13:08:38 +0100

    Hbase CDF with matplotlib and proper styling

---
+++ Python_plots/plots/cdf/hbase/hbase_seaborn_cdf.py
@@ -0,0 +1,159 @@
+import seaborn as sns
+import numpy as np
+import datetime
+import sys
+import os
+
+import matplotlib.pyplot as plt
+from matplotlib import rc
+
+rc('font', **{'family': 'sans-serif', 'sans-serif': ['Helvetica'],
+                  'serif': ['Helvetica'], 'size': 9})
+rc('text', usetex=True)
+rc('legend', fontsize=8)
+rc('axes', linewidth=0.5)
+rc('lines', linewidth=0.5)
+
+# paper_colors = ['#496ee2', '#8e053b', 'g', '#ef9708', '0', '#ff3399', '0.5', 'c', '0.7']
+colors = ['b', 'r', 'g', 'c', 'm']
+markers = ['o', '^', 'v', 'h']
+
+
+def plot_cdf(outname):
+    plt.ylim((0,1))
+    plt.xlim((-1,100))
+    plt.xlabel("Request latency [$m$s]")
+    plt.ylabel("CDF")
+    plt.grid(True)
+    plt.legend(loc=4, frameon=True, handlelength=2.5, handletextpad=0.2)
+    plt.savefig("%s.pdf"%outname, format="pdf", bbox_inches="tight")
+    plt.show()
+
+
+
+def cdf(data, min_val, max_val, label, label_count):
+
+    data_size=len(data)
+
+    # Set bins edges
+    data_set=sorted(set(data))
+    bins=np.append(data_set, data_set[-1]+1)
+
+    # Use the histogram function to bin the data
+    counts, bin_edges = np.histogram(data, bins=bins, density=False)
+
+    counts=counts.astype(float)/data_size
+
+    # Find the cdf
+    cdf = np.cumsum(counts)
+
+    # Plot the cdf
+    plt.plot(bin_edges[0:-1], cdf, linestyle='--', marker='o', label=label, color=colors[label_count])
+    # plt.plot([-100], [-100], label=label, color=colors[label_count], alpha=0.5)
+
+
+#############################################
+# Message Format:
+# {READ | INSERT | TOTAL},TIMESTAMP,LATENCY (us)
+#############################################
+def file_parser(fnames):
+    i = 0
+    for f in fnames:
+        print "Analyzing %s:" % (f)
+
+        # parsing
+        j = 0
+        minrto_outliers = 0
+        start_ts = 0
+        end_ts = 0
+        values = []
+        for line in open(f).readlines():
+            fields = [x.strip() for x in line.split(",")]
+            if fields[0] not in ["READ", "INSERT", "UPDATE"]:
+                if (fields[0] not in ["CLEANUP"]) and (fields[1] not in ["latency"]):
+                    print 'TYPE: %s -> NOT KNOWN '% fields[0]
+                continue
+            req_type = fields[0]
+            req_ts = datetime.datetime.fromtimestamp( float(fields[1]) / 1000.0)
+            req_latency = int(fields[2]) # Latency in micros
+            req_latency = int(req_latency/1000) # Convert to millis
+            if req_latency > 200:
+                minrto_outliers += 1
+            else:
+                values.append(req_latency)
+            #print "request: %s ts %s latency %d" % (req_type, str( req_ts), req_latency)
+
+            # Start and End timestamps
+            if j == 0:
+                start_ts = req_ts
+            elif j != 0:
+                end_ts = req_ts
+            j += 1
+        print "--------------------------------------"
+        print "%s (%s)" % (labels[i], f)
+        print "--------------------------------------"
+        print "%d total samples" % (j)
+        print "Runtime: %d seconds"% (end_ts-start_ts).total_seconds()
+        print "%d outliers due to MinRTO" % (minrto_outliers)
+        print "--------------------------------------"
+
+        # for type in ['READ', 'INSERT']:
+        if len(values) == 0:
+            continue
+        # print "===== TYPE: %s ====="% (type)
+        print "Throughput: %d req/sec" % (j / (end_ts - start_ts).total_seconds())
+        avg = np.mean(values)
+        print "AVG: %f" % (avg)
+        median = np.median(values)
+        print "MEDIAN: %f" % (median)
+        min_val = np.min(values)
+        print "MIN: %ld" % (min_val)
+        max_val = np.max(values)
+        print "MAX: %ld" % (max_val)
+        stddev = np.std(values)
+        print " STDEV: %f" % (stddev)
+        print " PERCENTILES:"
+        perc1 = np.percentile(values, 1)
+        print " 1st: %f" % (perc1)
+        perc10 = np.percentile(values, 10)
+        print " 10th: %f" % (np.percentile(values, 10))
+        perc25 = np.percentile(values, 25)
+        print " 25th: %f" % (np.percentile(values, 25))
+        perc50 = np.percentile(values, 50)
+        print " 50th: %f" % (np.percentile(values, 50))
+        perc75 = np.percentile(values, 75)
+        print " 75th: %f" % (np.percentile(values, 75))
+        perc90 = np.percentile(values, 90)
+        print " 90th: %f" % (np.percentile(values, 90))
+        perc99 = np.percentile(values, 99)
+        print " 99th: %f" % (np.percentile(values, 99))
+
+        cdf(values, min_val, max_val, labels[i], i)
+
+        i += 1
+
+
+if __name__ == '__main__':
+
+    print "Sytem Path {}".format(os.environ['PATH'])
+
+    if len(sys.argv) < 2:
+      print "Usage: memcached_latency_cdf.py <input file 1> <label 1> ... " \
+          "<input  file n> <label n> [output file]"
+
+    if (len(sys.argv) - 1) % 2 != 0:
+      outname = sys.argv[-1]
+    else:
+      outname = "hbase_latency_cdf"
+
+    fnames = []
+    labels = []
+    for i in range(0, len(sys.argv) - 1, 2):
+      fnames.append(sys.argv[1 + i])
+      labels.append(sys.argv[2 + i])
+
+    print 'Files given: {}'.format("".join(fname for fname in fnames))
+    print 'Labels given: {}'.format("".join(label for label in labels))
+
+    file_parser(fnames)
+    plot_cdf(outname)
