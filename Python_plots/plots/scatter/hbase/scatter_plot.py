fc34f43f4f826b793fdbb8df3a599782b2d3d9bf
pgaref <pgaref@macbook-air.lan>
2016-10-11 01:46:29 +0100

    Hbase scatter plot

--- Python_plots/plots/scatter/hbase/scatter_plot.py
+++ Python_plots/plots/scatter/hbase/scatter_plot.py
@@ -33,7 +33,28 @@ import os
 import matplotlib.pyplot as plt
 from matplotlib import rc
 
-plt.style.use('seaborn-white')
+
+# ## right panel
+# ax2 = fig.add_subplot(122)
+# props = dict(alpha=0.5, edgecolors='none' )
+#
+# handles = []
+# colors = ['blue', 'green', 'magenta', 'cyan']
+# for color in colors:
+#     x = np.random.randn(N)
+#     y = np.random.randn(N)
+#     s = np.random.randint(50,200)
+#     handles.append(ax2.scatter(x, y, c=color, s=s, **props))
+#
+# ax2.set_ylim([-5,11])
+# ax2.set_xlim([-5,11])
+#
+# ax2.legend(handles, colors)
+# ax2.grid(True)
+# ax2.set_aspect(1./ax2.get_data_ratio())
+
+
+# plt.style.use('seaborn-white')
 rc('font', **{'family': 'sans-serif', 'sans-serif': ['Helvetica'],
                   'serif': ['Helvetica'], 'size': 10})
 rc('text', usetex=True)
@@ -47,53 +68,59 @@ colors = ['b', 'r', 'g', 'c', 'm']
 
 
 # ALL DATA
-data = {}
-# data['workloadA'] = {}
+latency_data = {}
+throughput_data = {}
 
 # ALL workloads
-# workloads = ["A", "B", "C", "D", "E", "F"]
+workloads = ["A", "B", "C", "D", "E", "F"]
 systems_compared = ['YARN-Colocated','YARN-Cgroups','YARN-MEDEA']
-workloads = ["A", "B"]
+# workloads = ["A", "B"]
 
 
 def plot_boxes(outname):
-    fig, axes = plt.subplots(ncols=len(workloads), sharey=True)
-    fig.subplots_adjust(wspace=0)
-    # fig.text(0.5, 0.04, "YCSB Workloads", ha='center')
-    fig.text(0.04, 0.5, "Request latency [ms]", va='center', rotation='vertical')
 
-    for ax, name in zip(axes, workloads):
-        # whis from 5th to 99th precentile
-        ax.boxplot(x=[data[name][item] for item in systems_compared], whis=[5, 99], sym="+")
-        xtickNames = ax.set(xticklabels=systems_compared)
-        plt.setp(xtickNames, rotation=90, fontsize=8)
+    for name in workloads:
+        fig = plt.figure()
+        # fig, axes = plt.subplots(ncols=len(workloads), sharey=True)
+        # fig.subplots_adjust(wspace=0)
+        ax1 = fig.add_subplot(121)
+        ax1.set_xlabel("Latency 99th percentile [ms]")
+        ax1.set_ylabel("Throughput [ops/s]")
+        props = dict(alpha=0.5, edgecolors='none')
 
-        workloadXtick = ax.set(xlabel='workload'+name)
-        plt.setp(workloadXtick, fontsize=10)
-        # # Add a horizontal grid to the plot, but make it very light in color
-        # # so we can use it for reading data values but not be distracting
-        ax.yaxis.grid(True, linestyle='-', which='major', color='lightgrey',
-               alpha=0.5)
-        # Hide these grid behind plot objects
-        ax.set_axisbelow(True)
-        ax.margins(0.05) # Optional
-    # plt.ylim((0,50))
-    # plt.xlim((-1,100))
-    # plt.legend(loc=4, frameon=True, handlelength=2.5, handletextpad=0.2)
-    print "Done with plots"
-    plt.savefig("%s.pdf"%outname, format="pdf", bbox_inches="tight")
-    print "Done with Writing to file"
-    plt.show()
+        print "Latency Len: " + str(len(latency_data[name]['YARN-Colocated']))
+        print "Throughput len: "+ str(len(throughput_data[name]['YARN-Colocated']))
+
+        i = 0
+        handles = []
+        for item in systems_compared:
+            # s = np.random.randint(50,200)
+            handles.append(ax1.scatter(latency_data[name][item], throughput_data[name][item], color=colors[i], s=10, **props))
+            # ax1.scatter(latency_data["A"][item],throughput_data["A"][item], color=colors[i], s=5,edgecolor='none')
+            # ax1.set_aspect(1./ax1.get_data_ratio()) # make axes square
+            i += 1
+        ymin, ymax = ax1.get_ylim()
+        ax1.set_ylim(bottom=0, top=ymax)
+        ax1.set_xlim(0,250)
+        ax1.legend(handles, systems_compared)
+        ax1.grid(True)
+        ax1.set_aspect(1./ax1.get_data_ratio())
+
+        print "Done with plots"
+        plt.savefig("%s.pdf"%(outname+"_w"+name), format="pdf", bbox_inches="tight")
+        print "Done with Writing to file"
+        plt.clf()
+    # plt.show()
 
 
-
-def add_values(values, workload, label):
-    data[workload][label] = values
+def add_values(latency_values, throughput_values, workload, label):
+    latency_data[workload][label] = latency_values
+    throughput_data[workload][label] = throughput_values
 
 
 #############################################
 # Message Format:
-# {READ | INSERT | TOTAL},TIMESTAMP,LATENCY (us)
+# {READ | INSERT | UPDATE},TIMESTAMP,LATENCY (us)
 #############################################
 def file_parser(fnames, workload):
     i = 0
@@ -105,7 +132,13 @@ def file_parser(fnames, workload):
         minrto_outliers = 0
         start_ts = 0
         end_ts = 0
-        values = []
+
+        req_count = 0
+        current_ts = 0
+        all_values = []
+        latency_sum = []
+        latency_perc_values = []
+        throughput_values = []
         for line in open(f).readlines():
             fields = [x.strip() for x in line.split(",")]
             if fields[0] not in ["READ", "INSERT", "UPDATE"]:
@@ -119,7 +152,24 @@ def file_parser(fnames, workload):
             if req_latency > 200:
                 minrto_outliers += 1
             else:
-                values.append(req_latency)
+                all_values.append(req_latency)
+                if current_ts == 0:
+                    current_ts = req_ts
+                    latency_sum.append(req_latency)
+                    req_count = 1
+                elif (req_ts - current_ts).total_seconds() >= 1:
+                    latency_perc_values.append(np.percentile(latency_sum, 99))
+                    throughput_values.append(req_count)
+
+                    req_count = 1
+                    current_ts = req_ts
+                    latency_sum = []
+                    latency_sum.append(req_latency)
+                else:
+                    req_count += 1
+                    latency_sum.append(req_latency)
+
+
             #print "request: %s ts %s latency %d" % (req_type, str( req_ts), req_latency)
 
             # Start and End timestamps
@@ -137,35 +187,35 @@ def file_parser(fnames, workload):
         print "--------------------------------------"
 
         # for type in ['READ', 'INSERT']:
-        if len(values) == 0:
+        if len(all_values) == 0:
             continue
         # print "===== TYPE: %s ====="% (type)
         print "Throughput: %d req/sec" % (j / (end_ts - start_ts).total_seconds())
-        avg = np.mean(values)
+        avg = np.mean(all_values)
         print "AVG: %f" % (avg)
-        median = np.median(values)
+        median = np.median(all_values)
         print "MEDIAN: %f" % (median)
-        min_val = np.min(values)
+        min_val = np.min(all_values)
         print "MIN: %ld" % (min_val)
-        max_val = np.max(values)
+        max_val = np.max(all_values)
         print "MAX: %ld" % (max_val)
-        stddev = np.std(values)
+        stddev = np.std(all_values)
         print " STDEV: %f" % (stddev)
         print " PERCENTILES:"
-        perc10 = np.percentile(values, 10)
-        print " 10th: %f" % (np.percentile(values, 10))
-        perc25 = np.percentile(values, 25)
-        print " 25th: %f" % (np.percentile(values, 25))
-        perc50 = np.percentile(values, 50)
-        print " 50th: %f" % (np.percentile(values, 50))
-        perc75 = np.percentile(values, 75)
-        print " 75th: %f" % (np.percentile(values, 75))
-        perc90 = np.percentile(values, 90)
-        print " 90th: %f" % (np.percentile(values, 90))
-        perc99 = np.percentile(values, 99)
-        print " 99th: %f" % (np.percentile(values, 99))
+        perc10 = np.percentile(all_values, 10)
+        print " 10th: %f" % (np.percentile(all_values, 10))
+        perc25 = np.percentile(all_values, 25)
+        print " 25th: %f" % (np.percentile(all_values, 25))
+        perc50 = np.percentile(all_values, 50)
+        print " 50th: %f" % (np.percentile(all_values, 50))
+        perc75 = np.percentile(all_values, 75)
+        print " 75th: %f" % (np.percentile(all_values, 75))
+        perc90 = np.percentile(all_values, 90)
+        print " 90th: %f" % (np.percentile(all_values, 90))
+        perc99 = np.percentile(all_values, 99)
+        print " 99th: %f" % (np.percentile(all_values, 99))
 
-        add_values(values, workload, labels[i])
+        add_values(latency_perc_values, throughput_values, workload, labels[i])
 
         i += 1
 
@@ -181,7 +231,7 @@ if __name__ == '__main__':
     if (len(sys.argv) - 1) % 2 != 0:
       outname = sys.argv[-1]
     else:
-      outname = "hbase_latency_boxes"
+      outname = "hbase_latency_throughput_scatter"
 
     fpaths = []
     labels = []
@@ -197,7 +247,8 @@ if __name__ == '__main__':
         for path in  fpaths:
             fnames.append(path + "write-w"+workload+"-10R.dat")
         print "Processing.. "+ str(fnames)
-        data[workload] = {}
+        latency_data[workload] = {}
+        throughput_data[workload] = {}
         file_parser(fnames, workload)
 
     plot_boxes(outname)
