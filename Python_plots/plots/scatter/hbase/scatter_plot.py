e2d600fb06e58efb8d52a980b87a5f788ffea797
pgaref <pgaref@macbook-air.lan>
2016-10-11 01:47:32 +0100

    Cleaning

--- Python_plots/plots/scatter/hbase/scatter_plot.py
+++ Python_plots/plots/scatter/hbase/scatter_plot.py
@@ -34,25 +34,6 @@ import matplotlib.pyplot as plt
 from matplotlib import rc
 
 
-# ## right panel
-# ax2 = fig.add_subplot(122)
-# props = dict(alpha=0.5, edgecolors='none' )
-#
-# handles = []
-# colors = ['blue', 'green', 'magenta', 'cyan']
-# for color in colors:
-#     x = np.random.randn(N)
-#     y = np.random.randn(N)
-#     s = np.random.randint(50,200)
-#     handles.append(ax2.scatter(x, y, c=color, s=s, **props))
-#
-# ax2.set_ylim([-5,11])
-# ax2.set_xlim([-5,11])
-#
-# ax2.legend(handles, colors)
-# ax2.grid(True)
-# ax2.set_aspect(1./ax2.get_data_ratio())
-
 
 # plt.style.use('seaborn-white')
 rc('font', **{'family': 'sans-serif', 'sans-serif': ['Helvetica'],
