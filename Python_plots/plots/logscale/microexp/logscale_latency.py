5af852707c2de4585489b6ad8b46d8c7fe07764e
Panagiotis Garefalakis <pangaref@gmail.com>
2016-12-11 13:09:22 +0000

    Logscale Latency plot

--- Python_plots/plots/logscale/microexp/logscale_latency.py
+++ Python_plots/plots/logscale/microexp/logscale_latency.py
@@ -27,63 +27,29 @@ import os
 import numpy as np
 import pandas as pd
 import plots.utils as utils
+import brewer2mpl
+
+# brewer2mpl.get_map args: set name  set type  number of colors
+bmap = brewer2mpl.get_map('RdBu', 'Diverging', 4)
+# bmap = brewer2mpl.get_map('Set1', 'Qualitative', 4)
+colors = bmap.mpl_colors
+
 
 files = ["CPLEX-off_stats.csv", "CPLEX-on_stats.csv", "GR-NODE_CAND_stats.csv", "GR-SERIAL_stats.csv", "GR-RANDOM_stats.csv"]
 labels = ["ILP-offline", "ILP-online", "Node Candidates", "Random"]
 labels_map={"CPLEX-on": "ILP-online", "CPLEX-off": "ILP-offline",
             "GR-NODE_CAND": "Node Candidates", "GR-RANDOM": "Greedy"}
 
-hatch_patterns = ["", "/", "\\", "x", ".", "o", "O"]
-cluster_size = 100
+
+# colors = ['r', 'g', 'b', 'black', 'c', 'm']
+markers = ['o', '^', 'v', 'h', 'x']
+linestyle_list = ['--', '-.', ':', '-']
 
 # Global style configuration
 utils.set_rcs()
 
 
-def get_colors():
-    return np.array([
-        [0.1, 0.1, 0.1],          # black
-        [0.4, 0.4, 0.4],          # very dark gray
-        [0.7, 0.7, 0.7],          # dark gray
-        [0.9, 0.9, 0.9],          # light gray
-        [0.984375, 0.7265625, 0], # dark yellow
-        [1, 1, 0.9]               # light yellow
-    ])
-
-
-def color_bars(axes, colors):
-    i = 0
-    # These are the patches (matplotlib's terminology
-    # for the rectangles corresponding to the bars)
-    for p in axes.patches:
-        # Pull out the dark and light colors for
-        # the current subplot
-        dark_color = colors[i % len(labels)]
-        light_color = colors[(i + 1) % len(labels)]
-        # The first bar gets the dark color
-        # p1.set_color(dark_color)
-
-        # The second bar gets the light color, plus
-        # hatch marks int he dark color
-        p.set_color(light_color)
-        p.set_edgecolor(dark_color)
-        p.set_hatch(hatch_patterns[i % len(labels)])
-        i += 1
-
-
-def optimal_line_graph(formula, x_range):
-    x = np.array(x_range)
-    y = eval(formula)
-    utils.plt.plot(x, y, linestyle='--', linewidth=1.5, color='red')
-    utils.plt.show()
-
-
-def grouped_bar(data):
-    # ax = sns.barplot(
-    #     x='  totJobs', y='  ObjectiveValue ', hue='  Plan technique',
-    #     #order=["fixed", "reactive", "predictive"],
-    #     # hue_order=["oracle", "bayesian"],
-    #     data=data)
+def latency_logscale(data):
     fig = utils.plt.figure()
     ax = fig.add_subplot(111)
 
@@ -101,31 +67,30 @@ def grouped_bar(data):
     for cond in conditions:
         print "cond:", cond
         y_vals = data[data[:, 0] == cond][:, 2].astype(np.float)
+        x_vals = data[data[:, 0] == cond][:, 1].astype(np.int)
         pos = [j - (1 - space) / 2. + i * width for j in range(1, len(categories) + 1)]
         if labels_map.has_key(str(cond).strip()):
-            ax.bar(pos, y_vals, width=width, label=labels_map[str(cond).strip()], color=get_colors()[i],
-                   edgecolor=get_colors()[i+1],hatch=hatch_patterns[i])
+            ax.plot(x_vals, y_vals, label=labels_map[str(cond).strip()], color=colors[i], linestyle=linestyle_list[i],
+                    marker=markers[i], linewidth=1.5,)
+            # ,   edgecolor=get_colors()[i+1],hatch=hatch_patterns[i])
             i +=1
 
     indexes = range(1, len(categories) + 1)
     print "Indexes: ", indexes
     print "Categories: ", categories
-    ax.set_xticks(indexes)
-    ax.set_xticklabels(["10\%", "20\%", "30\%", "40\%", "50\%", "60\%", "70\%", "80\%", "90\%", "100\%"])
-    utils.plt.setp(utils.plt.xticks()[1], rotation=90)
 
     # Add the axis labels
-    ax.set_ylabel("Placement Efficiency")
-    ax.set_xlabel("Services Percentage")
+    ax.set_ylabel("Latency (ms)")
+    ax.set_xlabel("Number of Nodes")
 
-    # Show the 50% mark, which would indicate an equal
-    # ax.hlines(19.5, -0.5, 5.5, linestyle='--', linewidth=1)
-    optimal_line_graph('100*( x*8 ) + '+str(cluster_size) + '+ 100', range(0, len(categories) + 1))
-
+    # Make Y axis logscale
+    utils.plt.yscale('log', nonposy='clip')
     # Add a legend
     handles, labels = ax.get_legend_handles_labels()
     ax.legend(handles[::-1], labels[::-1])
     utils.plt.tight_layout()
+    # Create some space for the last marker
+    utils.plt.xlim((0, x_vals[len(x_vals)-1]+10))
 
     return fig, ax
 
@@ -134,9 +99,9 @@ def file_parser(fnames):
     file_data = (pd.read_csv(f) for f in fnames)
     all_data = pd.concat(file_data, ignore_index=True)
     # grouped_data = all_data.groupby(['  Plan technique', '  totJobs'])['  ObjectiveValue '].mean()
-    # # print all_data.columns.values
+    print all_data.columns.values
     # print grouped_data
-    numpyMatrix = all_data[['  Plan technique', '  totJobs','  ObjectiveValue ']].values
+    numpyMatrix = all_data[['  Plan technique', '  clusterSize', '  runTime(ms)']].values
     # print numpyMatrix
     return numpyMatrix
 
@@ -148,8 +113,7 @@ if __name__ == '__main__':
     if len(sys.argv) < 2:
         print "Usage: bars_efficiency.py.py <input PATH>"
         sys.exit(-1)
-
-    outname = "placement_efficiency_bars"
+    outname = "placement_latency_log"
 
     fpaths = []
     for file in files:
@@ -158,9 +122,10 @@ if __name__ == '__main__':
 
     print 'Files given: {}'.format(" | ".join(fname for fname in fpaths))
     # print 'Labels given: {}'.format(" | ".join(label for label in labels))
+    # print brewer2mpl.print_maps()
 
     data = file_parser(fpaths)
-    fig, axes = grouped_bar(data)
+    fig, axes = latency_logscale(data)
     utils.set_rcs()
-    utils.prepare_legend(legend_loc="upper left", legend_font=16)
+    utils.prepare_legend(legend_loc="upper left", legend_font=15)
     utils.writeout("%s"%outname)
